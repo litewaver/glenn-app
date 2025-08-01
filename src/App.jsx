8b4dcbfe470332627e26583b2d88cfb308b7fb24
victoria <josephv1791@gmail.com>
2025-07-31 23:00:30 -0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 import './App.css'
 import NavBar from './NavBar.jsx'
 import './index.css'
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 shadow-md fixed w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-lg font-bold">Glenn Coco</div>
         <ul className="flex space-x-4">
-          <li><a href="/" className="text-white hover:text-gray-300">Home</a></li>
+          <li><a href="/" className="text-white hover:text-green-300">Home</a></li>
           <li><a href="/reels" className="text-white hover:text-gray-300">Reels</a></li>
           <li><a href="/services" className="text-white hover:text-gray-300">Services</a></li>
           <li><a href="/about" className="text-white hover:text-gray-300">About</a></li>
